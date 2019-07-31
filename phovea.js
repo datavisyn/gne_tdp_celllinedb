151572b4d4eda2315f96e3fbbafff8ead31ba2fa
Mitchell Rezzonico <rezzonim@gene.com>
2019-07-31 16:55:21 -0700

    Add CellCentral and GeneHub proxy view

--- phovea.js
+++ phovea.js
@@ -5,7 +5,7 @@
  **************************************************************************** */
 
 //register all extensions in the registry following the given pattern
-module.exports = function(registry) {
+module.exports = function (registry) {
   /// #if include('extension-type', 'extension-id')	
   //registry.push('extension-type', 'extension-id', function() { return import('./src/extension_impl'); }, {});
   /// #endif
@@ -13,32 +13,61 @@ module.exports = function(registry) {
 
 
 
-  registry.push('ordinoStartMenuSubSection', 'gne_cellline_start', function() { return import('./src/menu/GNECellLineSubSection'); }, {
-  'name': 'GNE Cell Line',
-  'description': 'GNE Cell Line Panel',
-  'viewId': 'gne_cellline_list',
-  'idType': 'GNECellline',
-  'selection': 'none',
-  'cssClass': 'gne-cellline-entry-point'
- });
+  registry.push('ordinoStartMenuSubSection', 'gne_cellline_start', function () {return import('./src/menu/GNECellLineSubSection');}, {
+    'name': 'GNE Cell Line',
+    'description': 'GNE Cell Line Panel',
+    'viewId': 'gne_cellline_list',
+    'idType': 'GNECellline',
+    'selection': 'none',
+    'cssClass': 'gne-cellline-entry-point'
+  });
 
 
-  registry.push('tdpView', 'gne_cellline_list', function() { return import('./src/views/GNECelllineList'); }, {
-  'name': 'GNE Cell Line',
-  'idType': 'GNECellline',
-  'selection': 'none'
- });
+  registry.push('tdpView', 'gne_cellline_list', function () {return import('./src/views/GNECelllineList');}, {
+    'name': 'GNE Cell Line',
+    'idType': 'GNECellline',
+    'selection': 'none'
+  });
 
 
-  registry.push('tdpScore', 'expression_score', function() { return import('./src/scores/ExpressionScore'); }, {
-  'name': 'Expression Score',
-  'idtype': 'GNECellline'
- });
+  registry.push('tdpScore', 'expression_score', function () {return import('./src/scores/ExpressionScore');}, {
+    'name': 'Expression Score',
+    'idtype': 'GNECellline'
+  });
 
 
-  registry.push('tdpScoreImpl', 'expression_score', function() { return import('./src/scores/ExpressionScore'); }, {
-  'factory': 'new'
- });
+  registry.push('tdpScoreImpl', 'expression_score', function () {return import('./src/scores/ExpressionScore');}, {
+    'factory': 'new'
+  });
+
+
+  registry.push('tdpView', 'gne_cellcentral_proxy', function () {return import('./src/views/CellCentralProxy');}, {
+    'name': 'CellCentral',
+    'idType': 'GNECellline',
+    'selection': 'chooser',
+    'site': '//research.gene.com/cellcentral/#/cellline/{clid}',
+    'argument': 'clid',
+    'openExternally': true,
+    'factory': 'new',
+    'group': {
+      'name': 'GNE Resources',
+      // 'order': 0
+    }
+  });
+
+  registry.push('tdpView', 'gne_genehub_proxy', function () {return import('./src/views/GeneHubProxy');}, {
+    'name': 'GeneHub',
+    'idType': 'Ensembl',
+    'selection': 'chooser',
+    'site': '//research.gene.com/genehub/#/summary/gene/{ensg}/human',
+    'argument': 'ensg',
+    'openExternally': false,
+    'factory': 'new',
+    'group': {
+      'name': 'GNE Resources',
+      // 'order': 0
+    }
+  });
   // generator-phovea:end
 };
 
---
+++ src/views/GeneHubProxy.ts
@@ -0,0 +1,5 @@
+import ProxyView from 'tdp_core/src/views/ProxyView';
+
+export default class GeneHubProxy extends ProxyView {
+
+}
